e3e23faca3fe849aab5213aa4171d2d592aff8a5
harshith-km <harshithkm2003@gmail.com>
2026-01-31 01:53:45 +0530

    fixed product page style tage issue, integrated image caching

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -256,15 +256,17 @@ const ProductsPage = () => {
         />
       )}
 
-      <style jsx>{`
-        .scrollbar-hide::-webkit-scrollbar {
-          display: none;
-        }
-        .scrollbar-hide {
-          -ms-overflow-style: none;
-          scrollbar-width: none;
-        }
-      `}</style>
+      <style>
+        {`
+    .scrollbar-hide::-webkit-scrollbar {
+      display: none;
+    }
+    .scrollbar-hide {
+      -ms-overflow-style: none;
+      scrollbar-width: none;
+    }
+  `}
+      </style>
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        // Cache all images automatically
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg}"],
+      },
+    }),
+  ],
   base: "/veggi-cart/",
   server: {
     host: true,
